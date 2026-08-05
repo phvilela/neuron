803da269ab33df35b410f65f47a65fdf720ec3ff
Pedro Vilela <pedrovileladepaula@gmail.com>
2026-08-05 00:04:29 -0300

    Adiciona suporte para controle de usuário com crossterm

--- src/game.rs
+++ src/game.rs
@@ -1,15 +1,15 @@
 use std::{
-    cmp::{max, min},
-    io::{Write, stdout},
-    thread::sleep,
-    time::Duration,
+    cmp::{max, min}, io::{Error, ErrorKind::Other, Result, stdout}, thread::sleep, time::{Duration, Instant},
 };
 
+use crossterm::{cursor::MoveTo, event::{self, Event, KeyCode}, execute, terminal::{Clear, ClearType, disable_raw_mode, enable_raw_mode}};
 use rand::random_range;
 
 use crate::vector::Vector;
 
 const SLEEP_TIME: u64 = 10;
+
+const STDIO_MSG : &str = "Error handling stdio";
 
 pub struct Game {
     ball: Vector,
@@ -45,44 +45,94 @@ impl Game {
             y: rand_rad.sin(),
         };
         self.ball_speed = Game::DEFAULT_BALL_SPEED;
-
+        enable_raw_mode().expect(STDIO_MSG);
+        clear_screen().expect(STDIO_MSG);
         loop {
-            self.update();
+            if let Err(_) = self.update() { break;};
         }
+        disable_raw_mode().expect("Error restoring stdio mode");
     }
 
-    fn update(&mut self) {
+    
+    fn read_input(&mut self) -> Result<u8> {
+        let mut moves : u8 = 0;
+        let before = Instant::now();
+        while event::poll(Duration::ZERO)? {
+            if let Event::Key(key_pressed) = event::read()? {
+                match key_pressed.code {
+                    KeyCode::Esc => { return  Err(Error::new(Other,"Jogo Encerrado")); },
+                    KeyCode::Char('w') => { moves |= 0b11 ;},
+                    KeyCode::Char('s') => {moves |= 0b10;},
+                    KeyCode::Up => {moves |= 0b1100;},
+                    KeyCode::Down => {moves |= 0b1000;},
+
+                    _ => {}
+                }
+            }
+        }
+        sleep(Duration::from_millis(20) - (before - Instant::now()));
+        Ok(moves)
+    }
+
+    fn update_players(& mut self,moves: u8) {
+        if moves & 0b10 != 0 {
+            if moves & 0b01 == 0 {
+                self.player_a -= 1;
+            } else {
+                self.player_a += 1
+            }
+        } else if moves & 0b1000 != 0 {
+            if moves & 0b0100 == 0 {
+                self.player_b -= 1;
+            } else {
+                self.player_b += 1
+            }
+        }
+        let height = self.screen_height;
+        self.player_a = self.player_a.clamp(0, height.try_into().unwrap());
+        self.player_b = self.player_b.clamp(0, height.try_into().unwrap());
+    }
+    
+    fn update(&mut self) -> Result<()>{
+        let moves = self.read_input()?;
+        
+        self.update_players(moves);
         self.ball = self.ball.add(self.ball_direction.mul(self.ball_speed));
         self.check_colision();
         sleep(Duration::from_millis(SLEEP_TIME));
         self.render();
+        Ok(())
     }
 
     fn render(&self) {
-        clear_screen();
+        clear_screen().expect("");
         let x = self.ball.x;
         let y = self.ball.y;
         let ball = y as usize * self.screen_width as usize + x as usize;
         let mut line: usize = 0;
-        println!("Ball: {}\t Direction {}", self.ball, self.ball_direction);
-        println!("{}","-".repeat(self.screen_width as usize));
+        println!("Ball: {}\t A : {} B : {}", self.ball, self.player_a,self.player_b);
+        println!("\r{}", "-".repeat(self.screen_width as usize));
 
         loop {
-            if beetween( ball as isize, line as isize * self.screen_width, (line as isize + 1) * self.screen_width as isize ) {
+            if beetween(
+                ball as isize,
+                line as isize * self.screen_width,
+                (line as isize + 1) * self.screen_width as isize,
+            ) {
                 let mut print_string = " ".repeat(self.screen_width as usize);
                 let bytes_string = unsafe { print_string.as_bytes_mut() };
-                let ball_line_index = ball % self.screen_width as usize; 
-                if ball_line_index < self.screen_width as usize{
+                let ball_line_index = ball % self.screen_width as usize;
+                if ball_line_index < self.screen_width as usize {
                     bytes_string[ball_line_index] = 'o' as u8;
                 }
-                print!("{}",print_string);
+                print!("\r{}", print_string);
             }
-            
+
             line = line + 1;
             if line >= self.screen_height as usize {
                 break;
             }
-            println!()
+            println!("\r");
         }
     }
 
@@ -98,13 +148,13 @@ impl Game {
     }
 }
 
-fn clear_screen() {
-    // \x1B[2J limpa a tela inteira
-    // \x1B[1;1H move o cursor de volta para o canto superior esquerdo (linha 1, coluna 1)
-    print!("\x1B[2J\x1B[1;1H");
-
-    // É importante dar o flush para garantir que o comando seja executado imediatamente
-    stdout().flush().unwrap();
+fn clear_screen() -> Result<()> {
+    execute!(
+            stdout(),
+            Clear(ClearType::All), // Limpa toda a tela visível
+            MoveTo(0, 0)           // Move o cursor de volta para o canto superior esquerdo (1,1)
+        )?;
+    Ok(())
 }
 
 fn beetween(x: isize, a: isize, b: isize) -> bool {
